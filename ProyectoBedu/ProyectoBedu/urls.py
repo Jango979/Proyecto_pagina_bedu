c5ca3d5309a58f2facb8de58ab521d1265d2d591
Jango979 <37124142+Jango979@users.noreply.github.com>
2023-08-22 17:52:41 -0600

    applied changes

--- ProyectoBedu/ProyectoBedu/urls.py
+++ ProyectoBedu/ProyectoBedu/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ProyectoBedu.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('../../Pages/index.html'),
+    path('inicio/',inicio)
 ]
